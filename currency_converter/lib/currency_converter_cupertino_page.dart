7934a7a1a32fe18c73983e5278c4de010fed70c2
madhavi chaudhary <chaudharymadhavi184@gmail.com>
2024-06-12 18:19:07 +0530

    weather app

---
+++ currency_converter/lib/currency_converter_cupertino_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+
+class CurrencyConverterCupertinoPage extends StatefulWidget {
+  const CurrencyConverterCupertinoPage({super.key});
+
+  @override
+  State<CurrencyConverterCupertinoPage> createState() =>
+      _CurrencyConverterCupertinoPageState();
+}
+
+class _CurrencyConverterCupertinoPageState
+    extends State<CurrencyConverterCupertinoPage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //  print('rebuilt');
+
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.systemBlue,
+      navigationBar: const CupertinoNavigationBar(
+        backgroundColor: CupertinoColors.systemBlue,
+
+        middle: Text(
+          'Currency Converter',
+          style: TextStyle(
+            color: CupertinoColors.white,
+          ),
+        ),
+        // centerTitle: true,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'INR ${result != 0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0)}',
+              style: const TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CupertinoTextField(
+                controller: textEditingController,
+                style: const TextStyle(
+                  color: CupertinoColors.black,
+                ),
+                decoration: BoxDecoration(
+                  color: CupertinoColors.white,
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                placeholder: 'Please enter the amount in USD',
+                prefix: const Icon(CupertinoIcons.money_dollar),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: CupertinoButton(
+                color: CupertinoColors.black,
+                onPressed: () {
+                  setState(() {
+                    result = double.parse(textEditingController.text) * 81;
+                  });
+                },
+                child: const Text('Convert'),
+              ),
+            ),
+          ], //chindren
+        ),
+      ),
+    );
+  }
+}
